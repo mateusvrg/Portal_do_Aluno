ea77ccc289ec302c27f936cdc1c7203e1cb45762
Tweedy <ricardocrvg50@gmail.com>
2025-11-06 14:07:38 -0300

    add files andfolders

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+const app = express()
+
+const port_server = process.env.PORT_SERVER
+const port_app = process.env.PORT_APP
+
+app.use(express.json())
+
+app.use(cors({ credentials: true, origin: `http://localhost:${port_app}` }))
+
+//Routes
+// app.use('/users', UserRoutes)
+// app.use('/training', TrainingRoutes)
+// app.use('/diet', DietRoutes)
+
+app.listen(port_server)
+
