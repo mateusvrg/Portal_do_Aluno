b8fb26b2823c73b01f580dc5e332de771d384cf7
Mateus Virgílio <135419333+mateusvrg@users.noreply.github.com>
2025-11-17 17:55:30 -0300

    add edit files and remove login from AdminController

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -181,40 +181,6 @@ export default class AdminController {
     }
   }
 
-  // static async login(req, res) {
-  //   const email = req.body.email;
-  //   const password = req.body.password;
-
-  //   // validations
-  //   if (!email) {
-  //     res.status(422).json({ message: "O e-mail é obrigatório!" });
-  //     return;
-  //   }
-
-  //   if (!password) {
-  //     res.status(422).json({ message: "A senha é obrigatória!" });
-  //     return;
-  //   }
-
-  //   // check if user exists
-  //   const user = await User.findOne({ where: { email: email } });
-
-  //   if (!user) {
-  //     return res
-  //       .status(422)
-  //       .json({ message: "Não há usuário cadastrado com este e-mail!" });
-  //   }
-
-  //   // check if password match
-  //   const checkPassword = await bcrypt.compare(password, user.senha_hash);
-
-  //   if (!checkPassword) {
-  //     return res.status(422).json({ message: "Senha inválida" });
-  //   }
-
-  //   await createUserToken(user, req, res);
-  // }
-
   static async checkUser(req, res) {
     let currentUser;
 
